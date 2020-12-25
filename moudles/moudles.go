7693cb07fd1438e9307ff3b7edb8c19cc6e64cab
zhouzhijie3607154 <1843607154@qq.com>
2020-12-25 22:15:08 +0800

    parmas form parse\orm cloume parse \ json parse

--- moudles/moudles.go
+++ moudles/moudles.go
@@ -3,7 +3,7 @@ package moudles
 import "github.com/astaxie/beego/orm"
 
 //用户名结构体
-type User struct {
+type User struct {//form解析 orm映射的数据库表中列名解析 json 解析
 	Id       int    `form:"id" orm:"column(id)" json:"id"`
 	Username string `form:"username" orm:"column(user_name)" json:"username"`
 	Password string `form:"password" orm:"column(user_pwd)" json:"password"`
