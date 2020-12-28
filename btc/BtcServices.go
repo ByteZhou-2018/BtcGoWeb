93d7ad398dc14f0fc70e5f5b81be4bef1a511cbb
zhouzhijie3607154 <1843607154@qq.com>
2020-12-28 21:26:12 +0800

    add a func to get bitcoin core minner's difficultly

--- btc/BtcServices.go
+++ btc/BtcServices.go
@@ -4,6 +4,21 @@ import (
 	"BtcGoWeb/moudles"
 	"github.com/mitchellh/mapstructure"
 )
+
+//比特币节点命令 getblockchaininfo 的封装函数
+func GetBlockChainInfo()(*moudles.BlockChainInfo,error)  {
+	result, err := GetMsgByCommand("getblockchaininfo")
+	if err != nil {
+		return nil,err
+	}
+	var blockchain moudles.BlockChainInfo
+	err = mapstructure.Decode(result.Result, &blockchain)
+	if err != nil{
+		return nil,err
+	}
+	return &blockchain,nil
+}
+
 //比特币节点命令 getblockcount 的封装函数
 func GetBlockCount() (interface{}, error) {
 	result, err := GetMsgByCommand("getblockcount")
@@ -11,9 +26,17 @@ func GetBlockCount() (interface{}, error) {
 		return "", err
 	}
 	//存入数据
-	//o := orm.NewOrm()
-	//o.Insert(&result.Result)
+
+	//db.O.Insert(&result.Result)
 	return result.Result, err
+}
+//比特币节点命令 getdifficult 的封装函数
+func GetDifficult()(interface{}, error)  {
+	result, err := GetMsgByCommand("getdifficult")
+	if err != nil {
+		return nil,err
+	}
+	return result.Result,nil
 }
 //比特币节点命令 getbestblockhash 的封装函数
 func GetBestBlockHash() (interface{}, error) {
@@ -47,21 +70,7 @@ func GetBlockByHash(hash string) (*moudles.Blcok,error) {
 	return &block, nil
 }
 
-//比特币节点命令 getblockchaininfo 的封装函数
-func GetBlockChainInfo()(*moudles.BlockChainInfo,error)  {
-	result, err := GetMsgByCommand("getblockchaininfo")
-	if err != nil {
-		return nil,err
-	}
-	var blockchain moudles.BlockChainInfo
-	err = mapstructure.Decode(result.Result, &blockchain)
-	if err != nil{
-		return nil,err
-	}
-	return &blockchain,nil
-}
-
-//比特币节点命令 getblockheader 的封装函数
+//比特币节点命令 getblockheader hash的封装函数
 func GetBlockHeaderByHash(hash string) (*moudles.BlockHeader,error) {
 	result, err:= GetMsgByCommand("getblockheader", hash)
 	if err != nil {
